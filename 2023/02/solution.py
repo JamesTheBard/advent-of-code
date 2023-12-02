c0a84f619071f6e7e4027caea11c6c745b9b4ccc
JamesTheBard <jamesthebard@gmail.com>
2023-12-02 10:35:12 -0500

    Added 2023-02

---
+++ 2023/02/solution.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+from typing import Union, NamedTuple
+from operator import sub, gt, ge, lt, le
+from functools import reduce
+import re
+
+class Draw(NamedTuple):
+    red: int = 0
+    green: int = 0
+    blue: int = 0
+    
+    @property
+    def power(self):
+        return self.red * self.green * self.blue
+    
+    def __le__(self, other):
+        return all(le(i, j) for i, j in zip(self, other))
+    
+    def __add__(self, other):
+        return Draw(*(max(i, j) for i, j in zip(self, other)))
+
+
+Game = list[Draw]
+
+class Solution:
+    
+    games: list[Game]
+    input_file: Path
+    
+    def __init__(self, input_file: Union[str, Path]):
+        self.input_file = Path(input_file)
+        self.games: list[Game] = list()
+        self.process_input()
+        
+    def process_input(self) -> None:
+        data: list[str] = self.input_file.open('r').readlines()
+        for line in data:
+            game: Game = list()
+            for grab in line.split('; '):
+                draw_list = [i[::-1] for i in re.findall(r'(\d+) (\w+)', grab)]
+                game.append(Draw(**{k: int(v) for k, v in dict(draw_list).items()}))
+            self.games.append(game)
+            
+    def solve_part1(self, contents: Draw) -> int:
+        total = 0
+        for idx, game in enumerate(self.games):
+            if all(le(draw, contents) for draw in game):
+                total += (idx + 1)
+        return total
+    
+    def solve_part2(self) -> int:
+        return sum(reduce(lambda a, b: a + b, game).power for game in self.games)
+        
+
+if __name__ == "__main__":
+    s = Solution("input.txt")
+    bag_contents = Draw(red=12, green=13, blue=14)
+    print(s.solve_part1(bag_contents))
+    print(s.solve_part2())
