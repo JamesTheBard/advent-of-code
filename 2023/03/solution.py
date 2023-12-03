724a98ffedf1ecba4f1c884d2fa9118c76129ef4
JamesTheBard <jamesthebard@gmail.com>
2023-12-03 10:29:41 -0500

    Smidge of cleanup

--- 2023/03/solution.py
+++ 2023/03/solution.py
@@ -1,7 +1,7 @@
+import re
+from math import prod
 from pathlib import Path
 from typing import NamedTuple, Union
-from math import prod
-import re
 
 
 class Symbol(NamedTuple):
@@ -12,10 +12,10 @@ class Symbol(NamedTuple):
 
 class Number(NamedTuple):
     number: int
-    symbols: list[Symbol]
+    symbols: tuple[Symbol]
 
     @property
-    def gears(self):
+    def gears(self) -> tuple[Symbol]:
         return tuple(i for i in self.symbols if i.symbol == "*")
 
     def has_gear(self, gear: Symbol) -> bool:
@@ -23,13 +23,14 @@ class Number(NamedTuple):
 
 
 class Solution:
-    
+
     data: list[str]
     numbers: list[Number]
-    
+
     def __init__(self, input_file: Union[str, Path]):
         input_file: Path = Path(input_file)
-        self.data: list[str] = [i.strip() for i in input_file.open('r').readlines()]
+        self.data: list[str] = [i.strip()
+                                for i in input_file.open('r').readlines()]
         self.numbers: list[Number] = list()
         self.process_data()
 
@@ -37,10 +38,10 @@ class Solution:
         for idx, line in enumerate(self.data):
             if matches := re.finditer(r'(\d+)', line):
                 for m in matches:
-                    s: list[Symbol] = self.get_symbols(m.start(), m.end(), idx)
+                    s: tuple[Symbol] = self.get_symbols(m.start(), m.end(), idx)
                     self.numbers.append(Number(int(m.group(1)), s))
 
-    def get_symbols(self, x_start: int, x_end: int, y: int) -> list[Symbol]:
+    def get_symbols(self, x_start: int, x_end: int, y: int) -> tuple[Symbol]:
         symbols: list[Symbol] = list()
 
         x_start = x_start - 1 + (x_start == 0)
@@ -52,7 +53,7 @@ class Solution:
             for y in range(y_start, y_end + 1):
                 if self.data[y][x] not in ".0123456789":
                     symbols.append(Symbol(x, y, self.data[y][x]))
-        return symbols
+        return tuple(symbols)
 
     def solve_part1(self) -> int:
         return sum(i.number for i in self.numbers if i.symbols)
@@ -61,7 +62,8 @@ class Solution:
         total = 0
         gears: set[Symbol] = set(i.gears[0] for i in self.numbers if i.gears)
         for gear in gears:
-            numbers: tuple[Number] = tuple(i for i in self.numbers if i.has_gear(gear))
+            numbers: tuple[Number] = tuple(
+                i for i in self.numbers if i.has_gear(gear))
             if len(numbers) == 2:
                 total += prod(i.number for i in numbers)
         return total
