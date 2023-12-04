99784971512f43316900f3719abdd668252c4559
JamesTheBard <jamesthebard@gmail.com>
2023-12-04 09:09:55 -0500

    Added 2023-04

---
+++ 2023/04/solution.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+from typing import NamedTuple, Union
+
+
+class Ticket(NamedTuple):
+    winning_numbers: tuple[int]
+    ticket_numbers: tuple[int]
+
+    @property
+    def matches(self):
+        return len(set(self.winning_numbers) & set(self.ticket_numbers))
+
+    @property
+    def points(self):
+        return 2 ** (self.matches - 1) if self.matches else 0
+
+
+class Solution:
+    def __init__(self, input_file: Union[str, Path]):
+        input_file = Path(input_file)
+        self.tickets: dict[tuple[Ticket],
+                           int] = self.process_tickets(input_file)
+
+    def process_tickets(self, input_file: Path) -> tuple[Ticket]:
+        tickets = dict()
+        content = input_file.open('r').readlines()
+        for line in content:
+            line = line.split(': ')[-1].split('|')
+            winning_numbers = tuple(int(i) for i in line[0].split(' ') if i)
+            ticket_numbers = tuple(
+                int(i) for i in line[1].strip().split(' ') if i)
+            tickets[Ticket(winning_numbers, ticket_numbers)] = 1
+        return tickets
+
+    def solve_part1(self):
+        return sum(i.points for i in self.tickets.keys())
+
+    def solve_part2(self):
+        tickets: tuple[Ticket] = tuple(self.tickets.keys())
+        for idx, ticket in enumerate(tickets):
+            if not ticket.matches:
+                continue
+            for i in range(ticket.matches):
+                self.tickets[tickets[i + idx + 1]] += self.tickets[ticket]
+        return sum(i for i in self.tickets.values())
+
+
+if __name__ == "__main__":
+    s = Solution("input.txt")
+    print(s.solve_part1())
+    print(s.solve_part2())
