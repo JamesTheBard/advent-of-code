4bc1ff9dbc44a090329b2dde1390610fb4c412dd
JamesTheBard <jamesthebard@gmail.com>
2024-11-23 12:17:18 -0500

    2021/09 solve added

--- 2021/09/solution.py
+++ 2021/09/solution.py
@@ -1,50 +1,88 @@
+from functools import reduce
+from operator import mul
 from pathlib import Path
+from typing import Iterable
 
 
 class Solution:
 
-    input_file: Path
     data: list[str]
+    input_file: Path
+    height: int
+    width: int
 
     def __init__(self, input_file: str | Path):
         self.input_file = Path(input_file)
         self.data = self.process_input()
+        self.width = len(self.data[0])
+        self.height = len(self.data)
 
-    def process_input(self):
+    def process_input(self) -> list[str]:
         return [i.strip() for i in self.input_file.open('r').readlines()]
 
-    def is_minimum(self, x, y):
+    def find_neighbors(self, x, y) -> list[tuple[int, int]]:
+        candidates = [
+            (x - 1, y),
+            (x + 1, y),
+            (x, y - 1),
+            (x, y + 1),
+        ]
+        results = list()
+        for xx, yy in candidates:
+            if xx < 0 or xx >= self.width:
+                continue
+            if yy < 0 or yy >= self.height:
+                continue
+            results.append((xx, yy))
+        return results
+
+    def find_basin(self, x, y) -> set[tuple[int, int]]:
+        history = list()
+        todo = [(x, y)]
+        basin = [(x, y)]
+
+        previous_target = 0
+
+        while todo:
+            coordinate = todo.pop()
+            history.append(coordinate)
+            new_work = set(self.find_neighbors(*coordinate)).difference(history)
+            for x, y in new_work:
+                target = self.get_value(x, y)
+                if target == 9:
+                    history.append((x, y))
+                    continue
+                if target > previous_target:
+                    basin.append((x, y))
+                    todo.append((x, y))
+
+        return set(basin)
+
+    def get_value(self, x, y) -> int:
+        return int(self.data[y][x])
+
+    def get_minimums(self) -> Iterable[tuple[int, int]]:
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.is_minimum(x, y):
+                    yield (x, y)
+
+    def is_minimum(self, x, y) -> bool:
+        values = [self.data[j][i] for i, j in self.find_neighbors(x, y)]
         target = self.data[y][x]
-        if target == 9:
-            return False
-        width, height = len(self.data[0]) - 1, len(self.data) - 1
-        x1, x2 = max(0, x - 1), min(x + 1, width)
-        y1, y2 = max(0, y - 1), min(y + 1, height)
-
-        values = list(self.data[i][x1:x2 + 1] for i in range(y1, y2 + 1))
-        if len(values) == 2:
-            if y == 0:
-                values = ['9' * len(values[0])] + values
-            else:
-                values = values + ['9' * len(values[0])]
-        if len(values[0]) == 2:
-            if x == 0:
-                values = ['9' + i for i in values]
-            else:
-                values = [i + '9' for i in values]
-
-        values = sum((list(i) for i in values), [])
-        return min(values[:4] + values[5:]) > target
+        return min(values) > target
 
     def solve_part1(self):
-        total = 0
-        for y in range(len(self.data)):
-            for x in range(len(self.data[0])):
-                if self.is_minimum(x, y):
-                    total += 1 + int(self.data[y][x])
-        return total
-    
+        minimums = [self.get_value(*i) for i in self.get_minimums()]
+        return sum(minimums) + len(minimums)
+
+    def solve_part2(self):
+        basins = [len(self.find_basin(*i)) for i in self.get_minimums()]
+        basins = sorted(basins, reverse=True)
+        return reduce(mul, basins[:3])
+
 
 if __name__ == "__main__":
     s = Solution("input.txt")
     print(s.solve_part1())
+    print(s.solve_part2())
