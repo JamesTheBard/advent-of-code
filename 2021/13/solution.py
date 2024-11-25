ad223dca18229c8bb139e4000154c4225fdc43bd
JamesTheBard <jamesthebard@gmail.com>
2024-11-25 09:16:22 -0500

    2021/13 solve

--- 2021/13/solution.py
+++ 2021/13/solution.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 
 Coord = tuple[int, int]
+Fold = tuple[str, int]
+
 
 class Page:
+
+    points = set[Coord]
 
     def __init__(self, points: list[Coord]):
         self.points = set(points)
@@ -22,40 +26,54 @@ class Page:
             case _:
                 return self
 
+    @property
+    def text(self) -> str:
+        dim_x = max(x for x, _ in self.points)
+        dim_y = max(y for _, y in self.points)
+        text = [[" "] * (dim_x + 1) for _ in range(dim_y + 1)]
+        for x, y in self.points:
+            text[y][x] = "█"
+        return "\n".join((''.join(i) for i in text))
+
 
 class Solution:
+
+    folds = list[Fold]
+    input_file: Path
+    points = list[Coord]
 
     def __init__(self, input_file: str | Path):
         self.input_file = Path(input_file)
         self.points = list()
-        self.folds = dict()
+        self.folds = list()
         self.process_input()
 
-    def process_input(self):
+    def process_input(self) -> None:
         raw_data = [i.strip() for i in self.input_file.open('r').readlines()]
-        folds, points = list(), list()
-
         line_break = False
         for line in raw_data:
             if not line:
                 line_break = True
                 continue
-            if not line_break: points.append(tuple(int(i) for i in line.split(',')))
+            if not line_break:
+                self.points.append(tuple(int(i) for i in line.split(',')))
             if line_break:
                 data = line.split()[-1].strip().split('=')
-                folds.append((data[0], int(data[1])))
-        
-        self.folds = dict(folds)
-        self.points = points
-            
-    def solve_part1(self):
-        fold = list(self.folds.items())[0]
-        p = Page(self.points).fold(*fold)
-        return len(p.points)
+                self.folds.append((data[0], int(data[1])))
 
+    def solve_part1(self) -> int:
+        fold = self.folds[0]
+        page = Page(self.points).fold(*fold)
+        return len(page.points)
+
+    def solve_part2(self) -> str:
+        page = Page(self.points)
+        for fold in self.folds:
+            page = page.fold(*fold)
+        return page.text
 
 
 if __name__ == "__main__":
     s = Solution("input.txt")
-    s.process_input()
     print(s.solve_part1())
+    print(s.solve_part2())
